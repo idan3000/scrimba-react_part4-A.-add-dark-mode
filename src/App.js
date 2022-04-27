7c9b3af3a6709278095f4710e3038ed97dabb31f
idan3000 <idanring@gmail.com>
2022-04-27 10:55:17 +0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+const App = () => {
+  const [darkModeState, setDarkModeState] = useState(true);
+
+  const changeMode = () => setDarkModeState(!darkModeState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar darkMode={darkModeState} toggleDarkMode={changeMode} />
+      <Main darkMode={darkModeState} />
     </div>
   );
-}
+};
 
 export default App;
